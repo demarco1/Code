0049377ba507de5c7176240f6b7ef35dd85f09f2
Aranad <aran@organicdesign.co.nz>
2015-11-03 13:42:53 -0200

    fixed height

--- LigminchaGlobalApp/main.js
+++ LigminchaGlobalApp/main.js
@@ -5,10 +5,10 @@
  */
 String.prototype.ucfirst = function() {
 	return this.charAt(0).toUpperCase() + this.slice(1);
-}
+};
 String.prototype.ucwords = function() {
 	return this.split(' ').map(function(s) { return s.ucfirst(); }).join(' ');
-}
+};
 
 /**
  * Preload images
