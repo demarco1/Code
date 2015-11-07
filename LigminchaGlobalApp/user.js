6dfde92d620668c491340ba6d5a0b3ee831932d4
Aranad <aran@organicdesign.co.nz>
2015-11-07 14:01:17 -0200

    add users online

--- LigminchaGlobalApp/user.js
+++ LigminchaGlobalApp/user.js
@@ -17,3 +17,30 @@ lg.User = lg.GlobalObject.extend({
 lg.User.getCurrent = function() {
 	// TODO
 };
+
+lg.User.usersOnline = function(notself) {
+	var list = [];
+	var users = lg.select({type: LG_USER});
+	for(var i in users) {
+		if(users[i] !== lg.user) {
+			var user = users[i].attributes;
+			var sessions = lg.select({type: LG_SESSION, owner: user.id});
+			if(sessions.length) {
+				var name = user.data.realname;
+				name += ' (' + lg.getObject(user.ref1).data.name + ')';
+				list.push(name);
+			}
+		}
+	}
+	return list;
+};
+
+lg.User.chatMenu = function() {
+	var users = this.usersOnline();
+	if(users.length > 0) {
+		var html = 'Chat (' + users.length + ' user' + (users.length == 1 ? '' : 's') + ')&nbsp;&nbsp;▼<ul>';
+		for(var i in users) html += '<li>' + users[i] + '</li>';
+		html += '</ul>';
+		return html;
+	} else return 'There are no other users online';
+};
