51c1a833a5d5e50f68dc4aaed3c806470a460488
Aranad <aran@organicdesign.co.nz>
2015-11-07 15:57:31 -0200

    just use the iframe for now

--- LigminchaGlobalApp/main.js
+++ LigminchaGlobalApp/main.js
@@ -47,11 +47,6 @@ if(lg.session && typeof webSocket === 'object') {
 	// Render the toolbar
 	lg.template('global-toolbar', lg.user, '#toolbar');
 	$('#toolbar').fadeIn(300);
-
-	// test
-	if(mw.config.get('toolbar')) {
-		parent.jQuery('#aside').css('background-color','red');
-	}
 }
 
 // Populate the welcome notice depe	nding on if there's a session
