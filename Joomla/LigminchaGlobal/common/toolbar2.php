8bc619368773be223495924581f220c691c457df
Aranad <aran@organicdesign.co.nz>
2015-11-09 20:34:12 -0200

    anim back, fix loader img

--- Joomla/LigminchaGlobal/common/toolbar2.php
+++ Joomla/LigminchaGlobal/common/toolbar2.php
@@ -46,7 +46,7 @@ $lgToolbarHead = "<link rel=\"stylesheet\" href=\"http://{$lgGlobalAppDomain}/st
 		<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/fakemediawiki.js\"></script>
 		<script type=\"text/javascript\">
 			{$script}
-			window.$ = jQuery;
+			if($ === undefined) window.$ = jQuery;
 		</script>
 		<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/crypto.js\"></script>
 		<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/underscore.js\"></script>
