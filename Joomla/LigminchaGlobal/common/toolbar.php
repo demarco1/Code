d341f81d37eb0e8243c7b29cbe6c6b6d052e70ac
Aranad <aran@organicdesign.co.nz>
2015-11-09 14:23:17 -0200

    put it back

--- Joomla/LigminchaGlobal/common/toolbar.php
+++ Joomla/LigminchaGlobal/common/toolbar.php
@@ -24,7 +24,7 @@ if( !defined( 'LG_VERSION' ) ) {
 
 // Add the iframe requesting the toolbar with some spacing above
 $parent = urlencode( 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-$lgToolbarBody = "<iframe id=\"g_tb_if\" name=\"g_tb_if\" src=\"http://{$lgGlobalAppDomain}/toolbar.php/{$parent}\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>";
+$lgToolbarBody = "<iframe id=\"g_tb_if\" name=\"g_tb_if\" src=\"http://{$lgGlobalAppDomain}/toolbar.php?parent={$parent}\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>";
 $lgToolbarBody .= "<div id=\"g_tb\" style=\"position:absolute;z-index:1000;top:-28px;left:0;width:100%;height:28px;\"><div id=\"lg-toolbar\"></div></div>";
 $lgToolbarBody .= "<div style=\"padding:0;margin:0;height:15px;\"></div>";
 
