55f0a1339ceb0dcc5b5d80b79ad79ed42ef8810d
Aranad <aran@organicdesign.co.nz>
2015-11-10 10:23:47 -0200

    only define window.mw if not already defined

--- LigminchaGlobalApp/resources/fakemediawiki.js
+++ LigminchaGlobalApp/resources/fakemediawiki.js
@@ -1,14 +1,16 @@
 /**
  * Make MediaWiki environment look present for the websocket.js code
  */
-window.mw = {
-	data: {},
-	config: {
-		get: function(k, d) {
-			if(!(k in window.mw.data)) return d;
-			var val = window.mw.data[k];
-			if( val.charAt(0) === '{' || val.charAt(0) === '[' ) val = JSON.parse( val );
-			return val;
+if(window.mw === undefined) {
+	window.mw = {
+		data: {},
+		config: {
+			get: function(k, d) {
+				if(!(k in window.mw.data)) return d;
+				var val = window.mw.data[k];
+				if( val.charAt(0) === '{' || val.charAt(0) === '[' ) val = JSON.parse( val );
+				return val;
+			}
 		}
-	}
+	};
 };
