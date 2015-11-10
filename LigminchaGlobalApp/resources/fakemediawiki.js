e33054e9722c4cc207639f06504462c1954ea982
Aranad <aran@organicdesign.co.nz>
2015-11-10 11:21:21 -0200

    missing key for setter

--- LigminchaGlobalApp/resources/fakemediawiki.js
+++ LigminchaGlobalApp/resources/fakemediawiki.js
@@ -2,14 +2,18 @@
  * Make MediaWiki environment look present for the websocket.js code
  */
 if(window.mw === undefined) {
-	window.mw = { data: {}, config: {} };
-	window.mw.config.get = function(k, d) {
-		if(!(k in window.mw.data)) return d;
-		var val = window.mw.data[k];
-		if( val.charAt(0) === '{' || val.charAt(0) === '[' ) val = JSON.parse( val );
-		return val;
-	};
-	window.mw.config.set = function(k, v) {
-		window.mw.data = v;
+	window.mw = {
+		data: {},
+		config: {
+			get: function(k, d) {
+				if(!(k in window.mw.data)) return d;
+				var val = window.mw.data[k];
+				if( val.charAt(0) === '{' || val.charAt(0) === '[' ) val = JSON.parse( val );
+				return val;
+			},
+			set: function(k, v) {
+				window.mw.data[k] = v;
+			}
+		}
 	};
 };
