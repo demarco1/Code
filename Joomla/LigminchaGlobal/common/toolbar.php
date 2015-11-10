61f677526c430eeb7f76f8b58ca74a8fa0318868
Aranad <aran@organicdesign.co.nz>
2015-11-10 11:13:12 -0200

    debugging main

--- Joomla/LigminchaGlobal/common/toolbar.php
+++ Joomla/LigminchaGlobal/common/toolbar.php
@@ -39,27 +39,27 @@ $wgOut->addJsConfigVars( 'wsRewrite', true );
 
 // Generate the toolbar head content
 $lgToolbarHead = "<!-- LigminchaGlobal toolbar head start -->
-<link rel=\"stylesheet\" href=\"http://{$lgGlobalAppDomain}/styles/toolbar.css\" />
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/fakemediawiki.js\"></script>
-<script type=\"text/javascript\">
-	{$lgScript}//if($ === undefined) window.$ = jQuery;
-</script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/crypto.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/underscore.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/backbone.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/WebSocket/websocket.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/distributed.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/object.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/server.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/user.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/session.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/version.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/util.js\"></script>
-<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/main.js\"></script>
+	<link rel=\"stylesheet\" href=\"http://{$lgGlobalAppDomain}/styles/toolbar.css\" />
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/fakemediawiki.js\"></script>
+	<script type=\"text/javascript\">
+		{$lgScript}//if($ === undefined) window.$ = jQuery;
+	</script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/crypto.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/underscore.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/backbone.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/resources/WebSocket/websocket.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/distributed.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/object.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/server.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/user.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/session.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/version.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/util.js\"></script>
+	<script type=\"text/javascript\" src=\"http://{$lgGlobalAppDomain}/main.js\"></script>
 <!-- LigminchaGlobal toolbar head end -->";
 
 // Generate the toolbar body content
 $lgToolbarBody = "<!-- LigminchaGlobal toolbar body start -->
-<div id=\"g_tb\" style=\"position:absolute;z-index:1000;top:-28px;left:0;width:100%;height:28px;\"><div id=\"lg-toolbar\"></div></div>
-<div style=\"padding:0;margin:0;height:15px;\"></div>
+	<div id=\"g_tb\" style=\"position:absolute;z-index:1000;top:-28px;left:0;width:100%;height:28px;\"><div id=\"lg-toolbar\"></div></div>
+	<div style=\"padding:0;margin:0;height:15px;\"></div>
 <!-- LigminchaGlobal toolbar body end -->";
