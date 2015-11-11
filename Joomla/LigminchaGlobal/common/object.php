9e992b220296f55719d4af5744651826751747a5
Aranad <aran@organicdesign.co.nz>
2015-11-11 16:02:40 -0200

    debug update

--- Joomla/LigminchaGlobal/common/object.php
+++ Joomla/LigminchaGlobal/common/object.php
@@ -118,6 +118,7 @@ class LigminchaGlobalObject {
 			$this->modified = self::timestamp();
 
 			$sqlVals = $this->sqlValues( false );
+			lgDebug($sqlVals);
 			$db->setQuery( "UPDATE $table SET $sqlVals WHERE `id`=0x{$this->id}" );
 			$db->query();
 		}
