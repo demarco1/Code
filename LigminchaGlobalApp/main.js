d86f7e26c86cab10594838009fa8cdd3f414e41d
Aranad <aran@organicdesign.co.nz>
2015-11-12 08:18:44 -0200

    preload toolbar loader img too

--- LigminchaGlobalApp/main.js
+++ LigminchaGlobalApp/main.js
@@ -75,3 +75,4 @@ jQuery(function() {
  * Preload images
  */
 (new Image()).src = 'http://ligminchaglobal.organicdesign.co.nz/images/loader.gif';
+(new Image()).src = 'http://ligminchaglobal.organicdesign.co.nz/images/toolbar-loader.gif';
