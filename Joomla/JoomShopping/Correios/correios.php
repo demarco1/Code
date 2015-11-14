f5cbcce19d6b8d47773a77d308be3c5f01b135b0
Aranad <aran@organicdesign.co.nz>
2015-11-14 16:04:33 -0200

    oops, but in package making

--- Joomla/JoomShopping/Correios/correios.php
+++ Joomla/JoomShopping/Correios/correios.php
@@ -152,7 +152,7 @@ class plgSystemCorreios extends JPlugin {
 				if( array_key_exists( 'quantity', $item ) ) $item['product_quantity'] = $item['quantity'];
 				$items[$i] = self::arrayToObject( $item );
 			}
-			if( $item->weight ) $tmp[] = $item[$i];
+			if( $item[$i]->weight ) $tmp[] = $item[$i];
 		}
 		$items = $tmp;
 
