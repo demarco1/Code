30c95b9abcdcc052c684e49012f72d62f171ccca
Aranad <aran@organicdesign.co.nz>
2016-02-02 15:03:16 -0200

    crumbs

--- Joomla/FullIssue/site/fullissue.php
+++ Joomla/FullIssue/site/fullissue.php
@@ -1,15 +1,14 @@
 <?php
 $db = JFactory::getDbo();
-$id = $_GET['cat'];
+$id = array_key_exists( 'cat', $_GET ) ? $_GET['cat'] : false;
 
 // If the id is not numeric, treat it as an alias and get the numeric id fro that
-if( !is_numeric( $id ) ) {
+if( $id && !is_numeric( $id ) ) {
 	$query = $db->getQuery( true );
 	$query->select( 'id' )->from( '#__categories' )->where( 'alias="' . $id . '"' );
 	$db->setQuery( $query );	
-	if(	$res = $db->loadObjectList() ) {
-		foreach( $res as $row ) $id = $row->id;
-	} else {
+	if(	$row = $db->loadObject() ) $id = $row->id;
+	else {
 		echo "No catgegory found with alias \"$id\"!";
 		$id = false;
 	}
@@ -22,9 +21,17 @@ if( $id ) {
 	$query->select( '*' )->from( '#__categories' )->where( "id=$id" );
 	$db->setQuery( $query );
 	$row = $db->loadObject();
-	print_r($row);
+	$cat = $row->title;
 	$document = JFactory::getDocument();
-	$document->setTitle( $row->title );
+	$document->setTitle( "$cat (full issue)" );
+
+	// Crumbs
+	$app = JFactory::getApplication();
+	$pathway = $app->getPathway();
+	$path = $pathway->getPathway();
+	$path[] = 'Fullissue';
+	$path[] = $cat;
+	$pathway->setPathway( $path );
 
 	// Render the articles found in the category from it's numeric ID
 	$query = $db->getQuery( true );
