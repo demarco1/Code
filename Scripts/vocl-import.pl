6d31194579cbe6b3e0eb6669bad4cbbd0123f842
Aranad <aran@organicdesign.co.nz>
2016-02-02 18:54:23 -0200

    more skel

--- Scripts/vocl-import.pl
+++ Scripts/vocl-import.pl
@@ -7,7 +7,11 @@ use Cwd qw( realpath );
 use Time::HiRes;
 use Digest::MD5 qw( md5_base64 );
 
-$dir = dirname( realpath( __FILE__ ) );
+# Die if unoconv not found
+die "This script depends on unoconv!" unless qx( which unoconv );
+
+# Die if sender not valid
+$sender = $ENV{SENDER};
 
 # Make a tmp dir to work in
 $tmpDir = md5_base64( Time::HiRes::time . rand() );
@@ -15,13 +19,26 @@ $tmpDir =~ s/\W//g;
 $tmpDir = "/tmp/vocl-" . substr( $tmpDir, 1, 5 );
 mkdir $tmpDir;
 
-
-# Get the zip file out of the attachment and unzip into the tmp dir
+# Extract the attachments and copy them to the tmp dir
+$msg = $ARGV[0];
 
 # Find the Word doc, bail if none, or more than one
 
 # Convert the Word doc into HTML
 qx( unoconv -f html "$tmpDir/$file.docx" );
+
+# Upload the images
+
+# Parse the HTML
+open HTML, '<', "$tmpDir/$file.html" or bail( "Couldn't open \"$tmpDir/$file.html\" for reading!" );
+while(<HTML>) {
+
+
+
+
+}
+
+
 
 
 
