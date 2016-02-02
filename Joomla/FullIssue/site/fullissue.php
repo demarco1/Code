69ef8ab254e7099330f3ebaa1c0d49856f7c0039
Aranad <aran@organicdesign.co.nz>
2016-02-02 13:25:05 -0200

    resolve alias

--- Joomla/FullIssue/site/fullissue.php
+++ Joomla/FullIssue/site/fullissue.php
@@ -1,14 +1,35 @@
 <?php
 $db = JFactory::getDbo();
 $id = $_GET['cat'];
-$query = $db->getQuery(true);
-$query->select('*');
-$query->from('#__content');
-$query->where('catid="'.$id.'"');
-$db->setQuery((string)$query);
-$res = $db->loadObjectList();
-foreach($res as $r){
-    echo '<h3>'.$r->title.'</h3>';
-    echo $r->fulltext;
+
+// If the id is not numeric, treat it as an alias and get the numeric id
+if( !is_numeric( $is ) ) {
+	$query = $db->getQuery( true );
+	$query->select( 'created_user_id' );
+	$query->from( '#__categories' );
+	$query->where( 'alias="' . $id . '"' );
+	$db->setQuery((string)$query);	
+	if(	$res = $db->loadObjectList() ) {
+		foreach( $res as $row ) $id = $row->created_user_id;
+	} else {
+		echo "No catgegory found with alias \"$id\"!" )
+		$id = false;
+	}
+}
+
+// Render the article in the category ID
+if( $id ) {
+	$query = $db->getQuery( true );
+	$query->select( '*' );
+	$query->from( '#__content' );
+	$query->where( 'catid="' . $id . '"' );
+	$db->setQuery( (string)$query );
+	$res = $db->loadObjectList();
+	echo '<div class="fullissue">';
+	foreach( $res as $row ){
+		echo '<h3>' . $row->title . '</h3>';
+		echo $row->fulltext;
+	}
+	echo '</div>';
 }
 ?>
