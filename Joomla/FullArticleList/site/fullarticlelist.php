d5bb819218bab3fb8673aa6bf24530320c51463b
Aranad <aran@organicdesign.co.nz>
2016-02-02 10:19:21 -0200

    read articles from cat id

--- Joomla/FullArticleList/site/fullarticlelist.php
+++ Joomla/FullArticleList/site/fullarticlelist.php
@@ -1,3 +1,14 @@
 <?php
-print "Full article list for category " . $_GET['cat'];
+$db = JFactory::getDbo();
+$id = $_GET['cat'];
+$query = $db->getQuery(true);
+$query->select('*');
+$query->from('#__content');
+$query->where('catid="'.$id.'"');
+$db->setQuery((string)$query);
+$res = $db->loadObjectList();
+foreach($res as $r){
+    echo '<h3>'.$r->title.'</h3>';
+    echo $r->introtext;
+}
 ?>
