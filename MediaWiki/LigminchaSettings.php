2d56beffc959ec535c31181aabb5a0a063a92fa6
Aranad <aran@organicdesign.co.nz>
2016-03-24 10:16:05 +1300

    dont bounce for local

--- MediaWiki/LigminchaSettings.php
+++ MediaWiki/LigminchaSettings.php
@@ -9,22 +9,24 @@ $wgDefaultSkin          = 'monobook';
 $wgLanguageCode         = 'pt-br';
 
 // Bounce clear to https
-if( !array_key_exists( 'HTTPS', $_SERVER ) || $_SERVER['HTTPS'] != 'on' ) {
-	header( "Location: https://wiki.ligmincha.com.br" . $_SERVER['REQUEST_URI'] );
-	exit;
-}
+if( !odIsLocal() ) {
+	if( !array_key_exists( 'HTTPS', $_SERVER ) || $_SERVER['HTTPS'] != 'on' ) {
+		header( "Location: https://wiki.ligmincha.com.br" . $_SERVER['REQUEST_URI'] );
+		exit;
+	}
 
-// Bounce naked domain to account request page
-if( $_SERVER['REQUEST_URI'] == '/' ) {
-	header( "Location: https://wiki.ligmincha.com.br/Especial:Pedir_conta" );
-	exit;
-}
+	// Bounce naked domain to account request page
+	if( $_SERVER['REQUEST_URI'] == '/' ) {
+		header( "Location: https://wiki.ligmincha.com.br/Especial:Pedir_conta" );
+		exit;
+	}
 
-// Make red-link edits use Visual Editor
-if( array_key_exists( 'redlink', $_GET ) && array_key_exists( 'action', $_GET ) && $_GET['action'] == 'edit' ) {
-	$url = str_replace( 'action', 'veaction', $_SERVER['REQUEST_URI'] );
-	header( "Location: https://wiki.ligmincha.com.br$url" );
-	exit;
+	// Make red-link edits use Visual Editor
+	if( array_key_exists( 'redlink', $_GET ) && array_key_exists( 'action', $_GET ) && $_GET['action'] == 'edit' ) {
+		$url = str_replace( 'action', 'veaction', $_SERVER['REQUEST_URI'] );
+		header( "Location: https://wiki.ligmincha.com.br$url" );
+		exit;
+	}
 }
 
 // Permissions
