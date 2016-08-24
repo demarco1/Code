e757b52362cfe56fc7030194c6816f1ebe690505
Aranad <aran@organicdesign.co.nz>
2016-08-24 12:33:05 -0300

    case sensitivity doesn't work for pt

--- Joomla/JoomShopping/Correios/correios.php
+++ Joomla/JoomShopping/Correios/correios.php
@@ -293,7 +293,7 @@ class plgSystemCorreios extends JPlugin {
 
 		// Get the tracking costs for Nacional and Módico
 		$tracking = file_get_contents( "$correios/servicos-adicionais-nacionais" );
-		if( preg_match( '|<table class="conteudo-tabela">.+?<td><strong>REGISTRO NACIONAL.+?([1-9][0-9.,]+).+?<td><strong>REGISTRO MÓDICO.+?([1-9][0-9.,]+)|is', $tracking, $m ) ) {
+		if( preg_match( '|<table class="conteudo-tabela">.+?<td><strong>Registro nacional.+?([1-9][0-9.,]+).+?<td><strong>Registro módico.+?([1-9][0-9.,]+)|s', $tracking, $m ) ) {
 			$tracking = str_replace( ',', '.', $m[2] );
 
 			// Get the weight/costs table (starting at the 100-150 gram entry)
